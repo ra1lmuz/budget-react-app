d7bd2d70fed59640de09f1997d9e0fae5c30c969
Alan Bogov <avavion@gmail.com>
2023-01-20 14:23:47 +0300

    Add react-router-dom and new pages

---
+++ src/pages/Root.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+
+const Root = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default Root;
